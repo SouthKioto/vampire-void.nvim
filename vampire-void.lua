a3329bf6083361708dc4863b05790bc3359644d9
SouthKioto <bolisegabartek6@gmail.com>
2026-02-04 21:22:43 +0100

    dodanie integracji z lazy.nvim

--- vampire-void.lua
+++ vampire-void.lua
@@ -35,6 +35,22 @@ function M.setup()
     hl(0, "Search", { fg = "#ffffff", bg = "#880000" })
     hl(0, "FloatBorder", { fg = "#ff0000", bg = "NONE" })
     hl(0, "TelescopeBorder", { fg = "#ff0000" })
+
+    --Lazy
+    hl(0, "LazyNormal", { bg = "#050505", fg = "#e0e0e0" })
+    hl(0, "LazyBorder", { fg = "#ff0000" })
+    hl(0, "LazyTitle", { fg = "#ff0000", bold = true })
+    hl(0, "LazyButton", { bg = "#111111", fg = "#ff0000" })
+    hl(0, "LazyButtonActive", { bg = "#440000", fg = "#ffffff", bold = true })
+    hl(0, "LazyH1", { bg = "#ff0000", fg = "#000000", bold = true })
+    hl(0, "LazySpecial", { fg = "#ff5555" })
+    hl(0, "LazyReasonPlugin", { fg = "#ff3333" })
+    hl(0, "LazyDir", { fg = "#666666" })
+    hl(0, "LazyUrl", { fg = "#666666" })
+    hl(0, "LazyCommit", { fg = "#ff5555" })
+    hl(0, "LazyNoDiff", { fg = "#ff0000" })
+    hl(0, "LazyProgressDone", { fg = "#ff0000" })
+    hl(0, "LazyProgressTodo", { fg = "#222222" })
 end
 
 return M
